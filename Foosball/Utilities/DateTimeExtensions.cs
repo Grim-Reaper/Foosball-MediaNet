56bba44ad9b1a5b1a84dad36d71e46546082ec52
Ian Fonseca <ian.fo@media.net>
2016-10-11 15:50:09 +0530

    Fixed Typographical Errors

--- Foosball/Utilities/DateTimeExtensions.cs
+++ Foosball/Utilities/DateTimeExtensions.cs
@@ -5,11 +5,11 @@ namespace Foosball.Utilities
     public static class DateTimeExtensions
     {
         static string[] extensions = // 0 1 2 3 4 5 6 7 8 9 
-            { "th", "st", "nd", "rd", "th", "th", "th", "tn", "th", "th", 
+            { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", 
                 // 10 11 12 13 14 15 16 17 18 19 
-                "th", "th", "th", "th", "th", "th", "th", "tn", "th", "th", 
+                "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", 
                 // 20 21 22 23 24 25 26 27 28 29 
-                "th", "st", "nd", "rd", "th", "th", "th", "tn", "th", "th", 
+                "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", 
                 // 30 31 
                 "th", "st" 
             };
